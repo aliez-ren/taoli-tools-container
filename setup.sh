57e758ae80fad507d697044ecdc80575ec9cccbc
Aliez <8287771+aliez-ren@users.noreply.github.com>
2025-09-28 02:50:29 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 curl -fsSL https://get.docker.com | sh
 curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/chrome.json -o chromium.json
 docker rm -f taoli-tools-container
-docker run --name=taoli-tools-container  --security-opt seccomp=chromium.json -v $HOME/taoli-tools:/home/taoli/data -d ghcr.io/aliez-ren/taoli-tools-container:latest
+docker volume create taoli-tools-data
+docker run --name=taoli-tools-container  --security-opt seccomp=chromium.json -v taoli-tools-data:/home/taoli/data -d ghcr.io/aliez-ren/taoli-tools-container:latest
 docker logs -f taoli-tools-container
