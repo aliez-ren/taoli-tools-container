5fad4c896298eed20c1d979791f0f08940da16da
Aliez <8287771+aliez-ren@users.noreply.github.com>
2025-09-29 02:42:45 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ mkdir -p /etc/docker/seccomp/
 
 echo "{\"seccomp-profile\": \"/etc/docker/seccomp/chromium.json\"}" > /etc/docker/daemon.json
 
-curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/chromium.json -o /etc/docker/seccomp/chromium.json
+curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/chromium.json > /etc/docker/seccomp/chromium.json
 
 curl -fsSL https://get.docker.com | sh
 
-curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/compose.yml -o compose.yml
+curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/compose.yml > compose.yml
 
 openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -subj /CN=signer -addext 'subjectAltName=DNS:signer,IP:127.0.0.1' -out CERT.pem -keyout KEY.pem
 
