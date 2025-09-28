4e9a4be11fb5af78ba1c955f32410294c80938aa
Aliez <8287771+aliez-ren@users.noreply.github.com>
2025-09-29 02:28:47 +0900

    Update setup.sh
    update

--- setup.sh
+++ setup.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-curl -fsSL https://get.docker.com | sh
-
 mkdir -p /etc/docker/seccomp/
+
 echo "{\"seccomp-profile\": \"/etc/docker/seccomp/chromium.json\"}" > /etc/docker/daemon.json
+
 curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/chromium.json -o /etc/docker/seccomp/chromium.json
+
+curl -fsSL https://get.docker.com | sh
 
 curl -fsSL https://github.com/aliez-ren/taoli-tools-container/raw/refs/heads/main/compose.yml -o compose.yml
 
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -subj /CN=taoli-tools-signer -addext 'subjectAltName=DNS:taoli-tools-signer' -out CERT.pem -keyout KEY.pem
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -subj /CN=signer -addext 'subjectAltName=DNS:signer,IP:127.0.0.1' -out CERT.pem -keyout KEY.pem
 
 if [ ! -f keychain.toml ]; then
   echo " " > keychain.toml
 fi
 
-docker stack deploy -c compose.yml -d taoli
+docker swarm init
+
+docker stack deploy -c compose.yml -d taoli_tools
 
 rm keychain.toml
 
@@ -22,4 +26,4 @@ rm KEY.pem
 
 mv CERT.pem /mnt/
 
-docker service logs -f taoli_taoli-tools-container
+docker service logs -f taoli_tools_container
